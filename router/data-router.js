cb6424be81db02f91392f1cebcb0d360719b398b
synoize <suryavanshivam@gamil.com>
2025-04-09 22:53:39 +0530

    version 1.1

--- router/data-router.js
+++ router/data-router.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const courseController = require('../controllers/course-controller');
 const appController = require('../controllers/app-controller');
+const authMiddleware = require('../middlewares/auth-middleware');
+
 
 router.route('/app').get(appController.getAppData);
 
-router.route('/courses').get(courseController.getAllCourses);
-router.route('/courses/:id').get(courseController.getCourseById);
+router.route('/courses').get(authMiddleware, courseController.getAllCourses);
+router.route('/courses/:id').get(authMiddleware, courseController.getCourseById);
 
 module.exports = router;
