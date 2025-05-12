6adf5c1576432765d747eac1e85bb1caf29160f3
synoize <suryavanshivam@gamil.com>
2025-05-12 20:01:26 +0530

    server deploy

--- router/data-router.js
+++ router/data-router.js
@@ -4,9 +4,10 @@ const router = express.Router();
 const courseController = require('../controllers/course-controller');
 const appController = require('../controllers/app-controller');
 const authMiddleware = require('../middlewares/auth-middleware');
+const adminMiddleware = require('../middlewares/admin-middleware');
 
 
-router.route('/app').get(appController.getAppData);
+router.route('/app').get(adminMiddleware, appController.getAppData);
 
 router.route('/courses').get(authMiddleware, courseController.getAllCourses);
 router.route('/courses/:id').get(authMiddleware, courseController.getCourseById);
